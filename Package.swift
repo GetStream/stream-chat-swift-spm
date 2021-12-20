68a27738863eead041133b981fc67358971bbbb3
Pol Quintana <pol.quintana1@gmail.com>
2021-12-20 10:44:38 +0100

    Point to test release

--- Package.swift
+++ Package.swift
@@ -22,9 +22,9 @@ let package = Package(
     targets: [
         .binaryTarget(name: "StreamChat",
                       url: "https://github.com/GetStream/stream-chat-swift/releases/download/2.6.1/StreamChat.zip",
-                      checksum: "51c80feb1c106bcabbf5bb183a92e385d393be32b629cdf806edf238b98ccb18"),
+                      checksum: "b4c8a13383d425d7b57cfa4c800b3309594134a962b03c0ea12d229ac9f75548"),
         .binaryTarget(name: "StreamChatUI",
                       url: "https://github.com/GetStream/stream-chat-swift/releases/download/2.6.1/StreamChatUI.zip",
-                      checksum: "0b335af899e6eb644220772fdb69b04801d15899d7f5528805e9e515c9efd201")
+                      checksum: "fdd77b81ad91320e1e0c4e8ba692dde84ca5bb39831507db26346b72e93db884")
     ]
 )
