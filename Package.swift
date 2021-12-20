95350af88585ab688d31f42137de3308f72608ed
Pol Quintana <pol.quintana1@gmail.com>
2021-12-20 18:29:59 +0100

    Update Package.swift to point to 4.6.0

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "StreamChat",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/2.6.1/StreamChat.zip",
-                      checksum: "51c80feb1c106bcabbf5bb183a92e385d393be32b629cdf806edf238b98ccb18"),
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.6.0/StreamChat.zip",
+                      checksum: "9a1b3e5fc83e938f855a17a958723c335b674380ced2342964aa0755a76817fc"),
         .binaryTarget(name: "StreamChatUI",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/2.6.1/StreamChatUI.zip",
-                      checksum: "0b335af899e6eb644220772fdb69b04801d15899d7f5528805e9e515c9efd201")
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.6.0/StreamChatUI.zip",
+                      checksum: "acaa43965ce3c23349fbf77c105d91e8ce7d0280e63769b0e3624dd02641a614")
     ]
 )
