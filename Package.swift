1d315c93dd37f9ee64ad41a2404c0b5169c50ef9
runner <runner@Mac-1642514638514.local>
2022-01-18 14:58:19 +0000

    Bump 4.9.0

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "StreamChat",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.8.0/StreamChat.zip",
-                      checksum: "3706c3ad4b9737dc98b7f06afa1fd48a74c723f450b542199f3b75a277837295"),
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.9.0/StreamChat.zip",
+                      checksum: "144df7118e4e3fe7417be207ee670be37a7a5fc7166cd905e30de599f24ea224"),
         .binaryTarget(name: "StreamChatUI",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.8.0/StreamChatUI.zip",
-                      checksum: "f503331ae30c7e784c87c32d5270217ebb79dd7ead2fc067ac10037fc8d747ec")
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.9.0/StreamChatUI.zip",
+                      checksum: "6952c641bc2f50866c1e83030a8939cf9ae0bf88633d8da6d160b3a02a6c46ae")
     ]
 )
