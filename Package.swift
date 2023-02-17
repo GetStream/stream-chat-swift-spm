6b7ebb9b371ad9e162b9dbab0b79d9f05e87b9e3
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-02-17 12:57:53 +0000

    Bump 4.27.0

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "StreamChat",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.26.0/StreamChat.zip",
-                      checksum: "81c5fa12d83bf2d807ba7fea16492163f37eb5b2fd7635f55a343f2f6ca279b9"),
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.27.0/StreamChat.zip",
+                      checksum: "99e812a0a44d300315d915f8afb495d420bc2c7b3684559d4f03500bac78bac2"),
         .binaryTarget(name: "StreamChatUI",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.26.0/StreamChatUI.zip",
-                      checksum: "148aaa5d2f6782fd2bb0fa5710873a508609b68a8cd2456398f06544ab2f1364")
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.27.0/StreamChatUI.zip",
+                      checksum: "e8d75195cfab86cb71f29dd135ee49dc85a312d89474a22adbff3445e0a215f3")
     ]
 )
