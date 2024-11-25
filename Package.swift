dff125f19711e4ea2b5c565b6bf211cbff63b0ec
Alexey Alter-Pesotskiy <alex@testableapple.com>
2024-11-25 17:52:39 +0000

    Bump 4.67.0

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "StreamChat",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.66.0/StreamChat.zip",
-                      checksum: "4b7b4ea46fb56450c5a32323c77457d207494288db3322568baf95330190559a"),
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.67.0/StreamChat.zip",
+                      checksum: "cbd23a0163b36a34b4f5b9794b8986731e54f3063e11ec4e3670fa7568d83e05"),
         .binaryTarget(name: "StreamChatUI",
-                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.66.0/StreamChatUI.zip",
-                      checksum: "d69625f85e5ba9686aa73259d3bb3c7e4eb6df35ba2e969f98242bbc5d314d21")
+                      url: "https://github.com/GetStream/stream-chat-swift/releases/download/4.67.0/StreamChatUI.zip",
+                      checksum: "1d1cd9151885b7d48c8e18b4148defac16fe1590c3c23119dbec0c16c342a868")
     ]
 )
